953674a549f8db1f99c7c5e8e62bc54d71c1bc98
Devaraj_TutortDSA <77384364+DevarajAutomation@users.noreply.github.com>
2024-02-24 14:42:44 +0530

    Create variables.py

---
+++ variables.py
@@ -0,0 +1,13 @@
+# variable is like container which holds some value in it.
+ #String
+my_variable_1 = 'Hello' 
+#Integer
+my_variable_2 = 25 
+
+#Boolean Values
+my_variable_3 = True
+
+
+print(my_variable_1)
+print(my_variable_2)
+print(my_variable_3)
