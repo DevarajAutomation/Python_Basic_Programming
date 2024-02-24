6f511c87cdc3be947ef731a641fa575665d068e5
Devaraj_TutortDSA <77384364+DevarajAutomation@users.noreply.github.com>
2024-02-24 17:21:27 +0000

    Adding basic programs of while

--- the_while_loop.py
+++ the_while_loop.py
@@ -6,3 +6,17 @@ while (i<=10):
     print('The number is :' , i)  # The body of the loop
     i+=1   # The increment
 
+""" The basic program on while loop"""
+
+prompt='\n Tell me something!, I will repeat it to you: '
+
+prompt += '\n Enter "quit" to exit the game'
+
+message=""
+
+while (message != 'quit'):
+
+    message=input(prompt)
+
+    if message != 'quit':
+        print(message)
