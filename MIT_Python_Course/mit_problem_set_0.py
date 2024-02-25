a0e661774040f417340e27767b8ff5417b10067a
Devaraj_TutortDSA <77384364+DevarajAutomation@users.noreply.github.com>
2024-02-25 09:32:20 +0000

    Adding new folder of MIT study

---
+++ MIT_Python_Course/mit_problem_set_0.py
@@ -0,0 +1,11 @@
+import math
+from math import log2
+
+#Ask the user input to get the numbers
+
+x=int(input('Enter the first number: '))
+y=int(input('Enter the second number: '))
+
+print(x**y)
+
+print(log2(x))
