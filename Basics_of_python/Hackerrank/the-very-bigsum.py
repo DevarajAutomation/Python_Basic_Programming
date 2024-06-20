73c0112c225b1a8e13073172d8c29f8793cf4be2
Devaraj_TH <77384364+DevarajAutomation@users.noreply.github.com>
2024-06-20 13:32:00 +0000

    warmup

---
+++ Basics_of_python/Hackerrank/the-very-bigsum.py
@@ -0,0 +1,10 @@
+"""https://www.hackerrank.com/challenges/a-very-big-sum/problem"""
+
+def big_sum(ar):
+
+    return sum(ar)
+
+
+a=[1000000001, 1000000002, 1000000003, 1000000004, 1000000005]
+
+print(big_sum(a))
