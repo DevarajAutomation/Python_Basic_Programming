01456fe180f60e49e9a23d0f9911a9bc18c6224e
Devaraj_TH <77384364+DevarajAutomation@users.noreply.github.com>
2024-06-26 15:07:16 +0000

    changes

--- MIT_Python_Course/Mini-Project-Weatherapp/weather.py
+++ MIT_Python_Course/Mini-Project-Weatherapp/weather.py
@@ -19,20 +19,25 @@ class Weather:
 
     
     def post_data(self):
-
+        self.unit_of_measure='°C'
+        
         json_response=self.response.json()
         self.name=json_response['name']
         self.temp=json_response['main']['temp']
         self.temp_min=json_response['main']['temp_min']
         self.temp_max=json_response['main']['temp_max']
-        print(f"The temparature in {self.name} is {self.temp}°C")
-        print(f"The minimum temparature in {self.name} is {self.temp_min}°C")
-        print(f"The maximum temparature in {self.name} is {self.temp_max}°C")
+        print(f"The temparature in {self.name} is {self.temp}{self.unit_of_measure}")
+        print(f"The minimum temparature in {self.name} is {self.temp_min}{self.unit_of_measure}")
+        print(f"The maximum temparature in {self.name} is {self.temp_max}{self.unit_of_measure}")
+        self.cities=dict([(self.name,self.temp)])
 
-        
+        #self.cities['name']=self.name
+        #self.cities['temparature']=self.temp,self.unit_of_measure
+        print(self.cities)
 weather_1=Weather('BLR',12.97194000,77.59369000)
 weather_2=Weather('TMK',13.50000000,77.00000000)
 weather_3=Weather('CHN',13.08784000,80.27847000 )
 weather_1.post_data()
 weather_2.post_data()
 weather_3.post_data()
+
