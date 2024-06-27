5df42c2327dcd92fa67a717da6fb9d2a85f7ac3f
Devaraj_TH <77384364+DevarajAutomation@users.noreply.github.com>
2024-06-27 06:39:08 +0000

    warmup

---
+++ Basics_of_python/Basics/string-indexing.py
@@ -0,0 +1,22 @@
+test_string_1='Python is a '
+
+test_string_2='Programming Language'
+
+output="".join(test_string_2)
+
+output_1=test_string_1.strip('th')
+print(output_1)
+print(output)
+
+# a,b,c,d,e,f=test_string
+# k=2
+# #print(len(test_string))
+
+#for i in range(len(test_string)):
+    
+    #print(i,":",test_string[i])
+
+# last_index= test_string[k:] + test_string[:k] 
+# print(last_index)
+# print(test_string[0:5:2])
+# print(test_string[::-1])
