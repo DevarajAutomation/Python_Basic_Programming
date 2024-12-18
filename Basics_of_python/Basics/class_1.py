b67f39a976a15622297900b6a1e8b413a0aca6ea
Devaraj_TH <77384364+DevarajAutomation@users.noreply.github.com>
2024-12-18 14:46:36 +0000

    18-12-2024

---
+++ Basics_of_python/Basics/class_1.py
@@ -0,0 +1,17 @@
+print(2+3) #addition
+print(9-4) #subtraction
+print(3*5) #multiplication
+print(4/5) #divison
+print(5%4) #modulus
+print(5**2) #exponential
+
+print(5//2) # floor
+
+print(type(2))
+print(type(2.5))
+print(type([2,3]))
+print(type({2:3}))
+print(type((2,3)))
+print(type({2,3}))
+print(type('dev'))
+print(type(3==3))
---
+++ Basics_of_python/Basics/class_2_strings.py
@@ -0,0 +1,49 @@
+# my_str='d'
+# print(my_str)
+# print(len(my_str))
+
+# str_2='Hello World!'
+# print(len(str_2))
+# my_class = 2
+
+# multi_line=f"""
+
+# This is a python programming {my_class} 
+
+# """
+
+# print(multi_line)
+
+# """string concatenation"""
+
+# str_1 = "This is a first python"
+
+str_2 = " learning session"
+
+# print(str_1 + str_2)
+
+# """newline chars"""
+
+# print("Today is \n sunday")
+# print("Today is \t sunday") 
+# print("Today is \'sunday\' ")
+# print("Today is \\sunday\\ ")
+
+# """String formatting"""
+
+# print('I love %s' %str_2)
+# print('I love {}'.format(str_2))
+# print(f'I love {str_2}')
+
+"""String access"""
+str_2=list(str_2)
+print(len(str_2))
+print(str_2[-4])
+
+print(str_2[0:3])
+print(str_2[-7:-1])
+
+"""String reverse"""
+
+str_2_new=str_2[::-1]
+print(str_2_new)
