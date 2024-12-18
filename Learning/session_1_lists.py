39ccf082472455db7953e15867db869bf3e5c4c6
Devaraj_TH <77384364+DevarajAutomation@users.noreply.github.com>
2024-12-18 15:05:14 +0000

    new

---
+++ Learning/session_1_lists.py
@@ -0,0 +1,17 @@
+list_1=['lemon','mango','apple','orange','gauva','potato']
+# print(list_1)
+
+# print(list_1[-1])
+"""unpacking the list items"""
+# f1,f2,f3, *f = list_1
+
+# print(f3)
+
+"""list slicing"""
+
+# print(list_1[0:3])
+# print(list_1[3:])
+# print(list_1[1:4])
+print(list_1[1])
+last=len(list_1)-1
+print(list_1[last])
